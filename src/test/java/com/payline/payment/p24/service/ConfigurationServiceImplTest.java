61bd67d77ab51014cc57077873d8a862d959dbc7
Jan FLORENT <jan.florent@mythalesgroup.io>
2018-08-03 17:14:26 +0200

    tests refundService et configService

--- src/test/java/com/payline/payment/p24/service/ConfigurationServiceImplTest.java
+++ src/test/java/com/payline/payment/p24/service/ConfigurationServiceImplTest.java
@@ -1,7 +1,10 @@
 package com.payline.payment.p24.service;
 
+import com.payline.payment.p24.bean.rest.P24CheckConnectionRequest;
 import com.payline.payment.p24.utils.LocalizationService;
 import com.payline.payment.p24.utils.P24Constants;
+import com.payline.payment.p24.utils.P24HttpClient;
+import com.payline.payment.p24.utils.P24Path;
 import com.payline.pmapi.bean.configuration.AbstractParameter;
 import com.payline.pmapi.bean.configuration.ContractParametersCheckRequest;
 import com.payline.pmapi.bean.configuration.ContractParametersCheckRequest.CheckRequestBuilder;
@@ -9,29 +12,38 @@ import com.payline.pmapi.bean.configuration.PartnerConfiguration;
 import com.payline.pmapi.bean.configuration.ReleaseInformation;
 import com.payline.pmapi.bean.payment.ContractConfiguration;
 import com.payline.pmapi.bean.payment.PaylineEnvironment;
+import okhttp3.*;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import static org.mockito.Mockito.*;
+
 public class ConfigurationServiceImplTest {
     private String lang = "FR";
     private Locale locale = new Locale(lang);
+
+    HttpUrl url = new HttpUrl.Builder().scheme("http").host("host").addPathSegment("path").build();
+    Request request = new Request.Builder().url(url).build();
 
     @InjectMocks
     private ConfigurationServiceImpl configurationService = new ConfigurationServiceImpl();
 
     @Mock
     private LocalizationService localization;
+
+    @Mock
+    private P24HttpClient httpClient;
 
     @Before
     public void setUp() {
@@ -41,16 +53,81 @@ public class ConfigurationServiceImplTest {
 
     @Test
     public void getName() {
-        Mockito.when(localization.getSafeLocalizedString(Mockito.anyString(), Mockito.eq(locale))).thenReturn(lang);
+        when(localization.getSafeLocalizedString(anyString(), eq(locale))).thenReturn(lang);
         String result = configurationService.getName(locale);
         Assert.assertFalse(StringUtils.isEmpty(result));
         Assert.assertEquals(lang, result);
 
     }
 
+
     @Test
-    public void checkHttpConnection() {
+    public void checkHttpConnectionOK() throws IOException {
+        // create response object
+        ResponseBody bodyOK = ResponseBody.create(MediaType.parse("plain/text"), "error=0");
+        Response okResponse = new Response.Builder().code(200).request(request).protocol(Protocol.HTTP_2).body(bodyOK).message("foo").build();
+
+        when(httpClient.doPost(anyString(), any(P24Path.class), anyMap())).thenReturn(okResponse);
+
+        P24CheckConnectionRequest checkConnectionRequest = new P24CheckConnectionRequest(createContractParametersCheckRequest("a", "a", "a", "a"));
+
+        Map<String, String> errors = new HashMap<>();
+        configurationService.checkHttpConnection(checkConnectionRequest, errors, locale);
+
+        Assert.assertEquals(0, errors.size());
+
     }
+
+    @Test
+    public void checkHttpConnectionWithErrorCode() throws IOException {
+        // create response object
+        Response okResponse = new Response.Builder().code(500).request(request).protocol(Protocol.HTTP_2).message("foo").build();
+
+        when(httpClient.doPost(anyString(), any(P24Path.class), anyMap())).thenReturn(okResponse);
+
+        P24CheckConnectionRequest checkConnectionRequest = new P24CheckConnectionRequest(createContractParametersCheckRequest("a", "a", "a", "a"));
+
+        Map<String, String> errors = new HashMap<>();
+        configurationService.checkHttpConnection(checkConnectionRequest, errors, locale);
+
+        Assert.assertEquals(1, errors.size());
+    }
+
+
+    @Test
+    public void checkHttpConnectionWithErrorResponse() throws IOException {
+        // create response object
+        ResponseBody body = ResponseBody.create(MediaType.parse("plain/text"), "I am not the good response");
+        Response okResponse = new Response.Builder().code(200).request(request).protocol(Protocol.HTTP_2).body(body).message("foo").build();
+
+        when(httpClient.doPost(anyString(), any(P24Path.class), anyMap())).thenReturn(okResponse);
+
+        P24CheckConnectionRequest checkConnectionRequest = new P24CheckConnectionRequest(createContractParametersCheckRequest("a", "a", "a", "a"));
+
+        Map<String, String> errors = new HashMap<>();
+        configurationService.checkHttpConnection(checkConnectionRequest, errors, locale);
+
+        Assert.assertEquals(1, errors.size());
+    }
+
+
+    @Test
+    public void checkHttpConnectionWithException() {
+        try {
+            // create response object
+            when(httpClient.doPost(anyString(), any(P24Path.class), anyMap())).thenThrow(IOException.class);
+
+            P24CheckConnectionRequest checkConnectionRequest = new P24CheckConnectionRequest(createContractParametersCheckRequest("a", "a", "a", "a"));
+
+            Map<String, String> errors = new HashMap<>();
+            configurationService.checkHttpConnection(checkConnectionRequest, errors, locale);
+
+            Assert.assertEquals(1, errors.size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Test
     public void checkSoapConnection() {
@@ -62,25 +139,30 @@ public class ConfigurationServiceImplTest {
         Assert.assertEquals(10, parameters.size());
     }
 
-//    @Test
-//    public void check() {
-//        // test a good connection test
-//        String goodMerchantId = "65840";
-//        String goodPosId = "65840";
-//        String goodKey = "0f67a7fec13ff180";
-//        String goodPassword = "76feca7a92aee7d069e32a66b7e8cef4";
-//
-//        String notNumericMerchantId = "foo";
-//        String notNumericPosId = "bar";
-//
-//        ContractParametersCheckRequest request = createContractParametersCheckRequest(goodMerchantId, goodPosId, goodKey, goodPassword);
-//        Map errors = configurationService.check(request);
-//        Assert.assertEquals(0, errors.size());
-//
-//        request = createContractParametersCheckRequest(notNumericMerchantId, notNumericPosId, goodKey, goodPassword);
-//        errors = configurationService.check(request);
-//        Assert.assertEquals(2, errors.size());
-//    }
+    @Test
+    public void check() throws IOException {
+        // test a good connection test
+        String goodMerchantId = "65840";
+        String goodPosId = "65840";
+        String goodKey = "0f67a7fec13ff180";
+        String goodPassword = "76feca7a92aee7d069e32a66b7e8cef4";
+
+        String notNumericMerchantId = "foo";
+        String notNumericPosId = "bar";
+
+        ResponseBody bodyOK = ResponseBody.create(MediaType.parse("plain/text"), "error=0");
+        Response okResponse = new Response.Builder().code(200).request(request).protocol(Protocol.HTTP_2).body(bodyOK).message("foo").build();
+        when(httpClient.doPost(anyString(), any(P24Path.class), anyMap())).thenReturn(okResponse);
+
+
+        ContractParametersCheckRequest request = createContractParametersCheckRequest(goodMerchantId, goodPosId, goodKey, goodPassword);
+        Map errors = configurationService.check(request);
+        Assert.assertEquals(0, errors.size());
+
+        request = createContractParametersCheckRequest(notNumericMerchantId, notNumericPosId, goodKey, goodPassword);
+        errors = configurationService.check(request);
+        Assert.assertEquals(2, errors.size());
+    }
 
     @Test
     public void getReleaseInformation() {
--- src/test/java/com/payline/payment/p24/service/RefundServiceImplTest.java
+++ src/test/java/com/payline/payment/p24/service/RefundServiceImplTest.java
@@ -5,15 +5,71 @@ import com.payline.payment.p24.utils.SoapHelper;
 import com.payline.pmapi.bean.refund.request.RefundRequest;
 import com.payline.pmapi.bean.refund.response.RefundResponse;
 import com.payline.pmapi.bean.refund.response.impl.RefundResponseFailure;
+import com.payline.pmapi.bean.refund.response.impl.RefundResponseSuccess;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RefundServiceImplTest {
+    private String soapResponseOK = "<SOAP-ENV:Envelope SOAP-ENV:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"https://sandbox.przelewy24.pl/external/wsdl/service.php\" xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
+            "  <SOAP-ENV:Body>" +
+            "     <ns1:TrnRefundResponse>" +
+            "        <return xsi:type=\"ns1:TrnRefundResult\">" +
+            "           <result SOAP-ENC:arrayType=\"ns1:SingleRefund[1]\" xsi:type=\"ns1:ArrayOfSingleRefund\">" +
+            "              <item xsi:type=\"ns1:SingleRefund\">" +
+            "                 <orderId xsi:type=\"xsd:int\">890857</orderId>" +
+            "                 <orderIdFull xsi:type=\"xsd:int\">890857</orderIdFull>" +
+            "                 <sessionId xsi:type=\"xsd:string\">PL_TEST_2018-08-03_09-34-24</sessionId>" +
+            "                 <status xsi:type=\"xsd:boolean\">false</status>" +
+            "              </item>" +
+            "           </result>" +
+            "           <error xsi:type=\"ns1:GeneralError\">" +
+            "              <errorCode xsi:type=\"xsd:int\">0</errorCode>" +
+            "              <errorMessage xsi:type=\"xsd:string\">NoError</errorMessage>" +
+            "           </error>" +
+            "        </return>" +
+            "     </ns1:TrnRefundResponse>" +
+            "  </SOAP-ENV:Body>" +
+            "</SOAP-ENV:Envelope>";
+
+    InputStream isOK = new ByteArrayInputStream(soapResponseOK.getBytes());
+    SOAPMessage messageOK = MessageFactory.newInstance().createMessage(null, isOK);
+
+    private String soapResponseKO = "<SOAP-ENV:Envelope SOAP-ENV:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"https://sandbox.przelewy24.pl/external/wsdl/service.php\" xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
+            "  <SOAP-ENV:Body>" +
+            "     <ns1:TrnRefundResponse>" +
+            "        <return xsi:type=\"ns1:TrnRefundResult\">" +
+            "           <result SOAP-ENC:arrayType=\"ns1:SingleRefund[1]\" xsi:type=\"ns1:ArrayOfSingleRefund\">" +
+            "              <item xsi:type=\"ns1:SingleRefund\">" +
+            "                 <orderId xsi:type=\"xsd:int\">890857</orderId>" +
+            "                 <orderIdFull xsi:type=\"xsd:int\">890857</orderIdFull>" +
+            "                 <sessionId xsi:type=\"xsd:string\">PL_TEST_2018-08-03_09-34-24</sessionId>" +
+            "                 <status xsi:type=\"xsd:boolean\">false</status>" +
+            "              </item>" +
+            "           </result>" +
+            "           <error xsi:type=\"ns1:GeneralError\">" +
+            "              <errorCode xsi:type=\"xsd:int\">10000</errorCode>" +
+            "              <errorMessage xsi:type=\"xsd:string\">foo</errorMessage>" +
+            "           </error>" +
+            "        </return>" +
+            "     </ns1:TrnRefundResponse>" +
+            "  </SOAP-ENV:Body>" +
+            "</SOAP-ENV:Envelope>";
+
+    InputStream isKO = new ByteArrayInputStream(soapResponseKO.getBytes());
+    SOAPMessage messageKO = MessageFactory.newInstance().createMessage(null, isKO);
 
     @InjectMocks
     private RefundServiceImpl service;
@@ -21,17 +77,45 @@ public class RefundServiceImplTest {
     @Mock
     private SoapHelper soapHelper;
 
+    public RefundServiceImplTest() throws IOException, SOAPException {
+    }
+
     @Test
-    public void refundRequest() {
-        //Mockito.when(SoapHelper.sendSoapMessage(Mockito.any(SOAPMessage.class),Mockito.anyString())).thenReturn(null);
+    public void refundRequestOK() {
+        when(soapHelper.sendSoapMessage(any(SOAPMessage.class),anyString())).thenReturn(messageOK);
         RefundRequest paymentRequest = TestUtils.createRefundRequest("2");
 
         RefundResponse response = service.refundRequest(paymentRequest);
 
         Assert.assertNotNull(response);
-//        Assert.assertEquals(RefundResponseSuccess.class, response.getClass());
+        Assert.assertEquals(RefundResponseSuccess.class, response.getClass());
+    }
+
+    @Test
+    public void refundRequestWithSOAPError() {
+
+        when(soapHelper.sendSoapMessage(any(SOAPMessage.class),anyString())).thenReturn(null);
+        RefundRequest paymentRequest = TestUtils.createRefundRequest("2");
+
+        RefundResponse response = service.refundRequest(paymentRequest);
+
+        Assert.assertNotNull(response);
         Assert.assertEquals(RefundResponseFailure.class, response.getClass());
     }
+
+
+    @Test
+    public void refundRequestKO() {
+
+        when(soapHelper.sendSoapMessage(any(SOAPMessage.class),anyString())).thenReturn(messageKO);
+        RefundRequest paymentRequest = TestUtils.createRefundRequest("2");
+
+        RefundResponse response = service.refundRequest(paymentRequest);
+
+        Assert.assertNotNull(response);
+        Assert.assertEquals(RefundResponseFailure.class, response.getClass());
+    }
+
 
     @Test
     public void canMultiple() {
@@ -42,16 +126,5 @@ public class RefundServiceImplTest {
     public void canPartial() {
         Assert.assertFalse(service.canPartial());
     }
-
-//    @Test
-//    public void getRefundResponseFailure() {
-//        String errorCode = "foo";
-//        FailureCause failureCause = FailureCause.INVALID_DATA;
-//
-//        RefundResponseFailure response = service.getRefundResponseFailure(errorCode, failureCause, "0");
-//        Assert.assertEquals(errorCode, response.getErrorCode());
-//        Assert.assertEquals(failureCause, response.getFailureCause());
-//        Assert.assertEquals("0", response.getTransactionId());
-//    }
 
 }
